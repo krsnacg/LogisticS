cf57846c93104e60de070b6a8248daccc3a9254a
gabriel <gabriel.cruz2@unmsm.edu.pe>
2024-11-22 10:05:56 -0500

    Fix possible null product fields

--- app/src/main/java/com/example/logistics/data/ProductMapper.kt
+++ app/src/main/java/com/example/logistics/data/ProductMapper.kt
@@ -6,11 +6,11 @@ import com.example.logistics.model.ProductResponse
 fun ProductResponse.toProduct(): Product {
     return Product(
         codigo = code,
-        nombreProducto = name,
+        nombreProducto = name ?: "",
         categoria = category.name,
         tipo = formaFarmaceutica.forma,
-        precio = price.toString(),
-        concentracion = concentracion,
+        precio = price.toString() ?: "",
+        concentracion = concentracion ?: "",
         presentacion = presentation ?: "",
         descripcion = description ?: "",
         cantidad = ""
