59623b802c63398cea3ecd638e856525635444c8
JamilTuncarQ <jamil.tuncar@unmsm.edu.pe>
2024-11-28 12:22:02 -0500

    Avance IU Registro pedido.2

--- app/src/main/java/com/example/logistics/ui/Pedido/AgregarPedidos.kt
+++ app/src/main/java/com/example/logistics/ui/Pedido/AgregarPedidos.kt
@@ -1,30 +1,15 @@
-package com.example.logistics.util.util_pedidos
+package com.example.logistics.ui.Pedido
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.icons.filled.Stars
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -32,14 +17,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.logistics.R
 import com.example.logistics.ui.theme.LogisticSTheme
+import com.example.logistics.util.util_pedidos.NavigationBarRecepcionista
+import com.example.logistics.util.util_pedidos.TopBar
 
 @Composable
 fun RegistroPedido(
     modifier: Modifier = Modifier,
     ){
     Scaffold (topBar = {
-        TopBar(tituloPagina = stringResource(R.string.topbar_opcion2), modo = "Retroceder")},
-        bottomBar = { NavigationBarRecepcionista(opcionSeleccionada = 2)}
+        TopBar(tituloPagina = stringResource(R.string.topbar_opcion2), modo = "Retroceder")
+    },
+        bottomBar = { NavigationBarRecepcionista(opcionSeleccionada = 2) }
     ){paddingValues ->
         LazyColumn (
             modifier = Modifier
